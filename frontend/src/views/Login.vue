eed250c344988a0c3cf1efa8bd5b9adbc27f27d3
aldy sufri <aldisu30@gmail.com>
2026-01-22 15:27:52 +0700

    refactor(frontend): login form

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,19 +3,21 @@
         <h3 class="mb-3 text-center">Login</h3>
 
         <div class="card p-4">
-            <div class="mb-3">
-                <label>Email</label>
-                <input type="email" class="form-control" v-model="form.email" />
-            </div>
-
-            <div class="mb-3">
-                <label>Password</label>
-                <input type="password" class="form-control" v-model="form.password" />
-            </div>
-
-            <button class="btn btn-primary w-100" @click="login">Login</button>
-
-            <p v-if="error" class="text-danger mt-3">{{ error }}</p>
+            <form @submit.prevent="login">
+                <div class="mb-3">
+                    <label>Email</label>
+                    <input type="email" class="form-control" v-model="form.email" />
+                </div>
+    
+                <div class="mb-3">
+                    <label>Password</label>
+                    <input type="password" class="form-control" v-model="form.password" />
+                </div>
+    
+                <button type="submit" class="btn btn-primary w-100">Login</button>
+    
+                <p v-if="error" class="text-danger mt-3">{{ error }}</p>
+            </form>
         </div>
     </div>
 </template>
@@ -39,7 +41,7 @@
             const res = await api.post('/login', form.value)
 
             localStorage.setItem('token', res.data.access_token)
-            
+
             router.push('/task')
         } catch (err) {
             error.value = 'Login gagal, email atau password salah'
