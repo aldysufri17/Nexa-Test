8fa894ba84f2f9d6e773057c60a8121f6bb79f00
aldy sufri <aldisu30@gmail.com>
2026-01-22 14:02:06 +0700

    feat(frontend): consume api Login using axios

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -5,17 +5,43 @@
         <div class="card p-4">
             <div class="mb-3">
                 <label>Email</label>
-                <input type="email" class="form-control" />
+                <input type="email" class="form-control" v-model="form.email" />
             </div>
 
             <div class="mb-3">
                 <label>Password</label>
-                <input type="password" class="form-control" />
+                <input type="password" class="form-control" v-model="form.password" />
             </div>
 
-            <button class="btn btn-primary w-100" >Login</button>
+            <button class="btn btn-primary w-100" @click="login">Login</button>
 
-            <p class="text-danger mt-3"></p>
+            <p v-if="error" class="text-danger mt-3">{{ error }}</p>
         </div>
     </div>
 </template>
+
+<script setup>
+    import axios from 'axios'
+    import { ref } from 'vue'
+
+    const form = ref({
+        email: '',
+        password: ''
+    })
+
+    const api = axios.create({
+        baseURL: 'http://127.0.0.1:8099/api',
+    })
+
+    const error = ref('')
+
+    const login = async () => {
+        try {
+            const res = await api.post('/login', form.value)
+
+            localStorage.setItem('token', res.data.access_token)
+        } catch (err) {
+            error.value = 'Login gagal, email atau password salah'
+        }
+    }
+</script>
