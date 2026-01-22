44e2d6a1a02ab682acc32e9d8ee881389c95b570
aldy sufri <aldisu30@gmail.com>
2026-01-22 11:53:15 +0700

    feat(frontend): slicing login page using boostrap

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,3 +1,21 @@
 <template>
-    <div>Hi login Page</div>
+    <div class="container mt-5" style="max-width:400px">
+        <h3 class="mb-3 text-center">Login</h3>
+
+        <div class="card p-4">
+            <div class="mb-3">
+                <label>Email</label>
+                <input type="email" class="form-control" />
+            </div>
+
+            <div class="mb-3">
+                <label>Password</label>
+                <input type="password" class="form-control" />
+            </div>
+
+            <button class="btn btn-primary w-100" >Login</button>
+
+            <p class="text-danger mt-3"></p>
+        </div>
+    </div>
 </template>
